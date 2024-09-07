07e87c091cc702054fd31454f58f907c3dbb54bb
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-09-06 23:31:36 -0700

    Delete getTextbookSections.js

--- getTextbookSections.js
+++
@@ -1,3 +0,0 @@
-// This snippet is meant to be pasted in the Inspect Element console of Portal's Textbook Viewer
-
-document.querySelectorAll('#contentListing [id]').forEach((element) => {if (element.id.startsWith("label-")) {console.log(element.id.substring(6))}})
