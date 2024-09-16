d4114f09ab33f5944bafa8f9ab025f9bff7fc986
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-09-15 19:09:10 -0700

    Update package.sh

--- package.sh
+++ package.sh
@@ -12,7 +12,7 @@ i=1
 
 for file in *; do
 	if [ -d "$file" ]; then
-		7z a -tzip -mm=Zstd -mx22 -w "../Kats_Bookshelf.$(date -I).Part_${i}_of_${parts}.Portal.zip" "$file/*"
+		7z a -tzip -mm=Zstd -mx22 -w "Kats_Bookshelf.$(date -I).Part_${i}_of_${parts}.Portal.zip" "$file/*"
 		i=$((i+1))
 	fi
 done
